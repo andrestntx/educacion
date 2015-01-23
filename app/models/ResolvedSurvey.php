b28e43c83fc5b51a006b9f365471f13eabcd603a
andres pinzon <andresmaopinzon@gmail.com>
2015-01-23 16:18:29 -0500

    Prueba de drivers

--- app/models/ResolvedSurvey.php
+++ app/models/ResolvedSurvey.php
@@ -61,14 +61,11 @@ class ResolvedSurvey extends Eloquent
         $survey_name = $this->survey->name;
         $pdf_path = $this->pdf;
 
-        try {
-            Mail::queue('emails.forms.new', compact('company_logo', 'company_name', 'survey_name', 'pdf_path'), function($message) use ($pdf_path, $user) {
-                $message->to($user->email, $user->name)->subject('Haz realizado un registro de Formulario');
-                $message->attach($pdf_path);
-            });  
-        } catch (Exception $e) {
-            
-        }
+        Mail::queue('emails.forms.new', compact('company_logo', 'company_name', 'survey_name', 'pdf_path'), function($message) use ($pdf_path, $user) {
+            $message->to($user->email, $user->name)->subject('Haz realizado un registro de Formulario');
+            $message->attach($pdf_path);
+        });  
+
                  
     }
 
